8a4e6cae5b9d90e2fc2e5281753c711ff2a67268
yayamada <damayamadama2@yahoo.co.jp>
2017-11-21 23:39:03 +0900

    no message

--- script/kaitoScript01.js
+++ script/kaitoScript01.js
@@ -1,10 +1,41 @@
 // JavaScript source code
-//alert('hello');
+
+//fncalert01‚Ì•Ï”
+var turnCount = 0;
+//fncalert02•Ï”
+var turnCount02 = 0;
+var beforeNumber = 0;
+var afterNumber = 0;
 
 function aaa(t) {
     alert(t);
 }
 
 function fncalert(number) {
-    alert(number);
+    if (number % 2 == 0) {
+        alert('‹ô”');
+    } else {
+        alert('Šï”');
+    }
+}
+
+function fncalert01(number) {
+    turnCount++;
+    if (turnCount % 2 == 0) {
+        alert(turnCount);
+    }
+}
+
+function fncalert02(number) {
+    turnCount02++;
+    if (turnCount02 % 2 == 1) {
+        beforeNumber = number;
+    } else {
+        afterNumber = number;
+        if (beforeNumber == afterNumber) {
+            alert('Just do it!');
+        } else {
+            alert('See you later.');
+        }
+    }
 }
