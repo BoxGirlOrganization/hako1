dcb08f4575935fd401217f4b17d24bc219c2a17a
DESKTOP-8B9VD2B\chiaki <chiaki77t@gmail.com>
2017-11-21 23:14:51 +0900

    button alert

---
+++ script/chiakiScript01.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+//alert('hello');
+
+function aaa(t) {
+    alert(t);
+}
+
+function fncalert(number) {
+	if ( number % 2 == 0) {
+		alert('‹ô”');
+	} else {
+	   alert('Šï”');
+	}
+}
